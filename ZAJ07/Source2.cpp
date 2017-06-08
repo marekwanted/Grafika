96f0e82ce205c96d3905c08c67fd45814c686a40
Marek <newwanted@windowslive.com>
2017-06-08 22:32:21 +0200

    Added project ZAJ06 and source file

--- ZAJ07/Source2.cpp
+++ ZAJ07/Source2.cpp
@@ -1,11 +1,25 @@
+
+
 /*
 (c) Janusz Ganczarski
 http://www.januszg.hg.pl
 JanuszG@enter.net.pl
 */
 
-#include <stdlib.h>
-#include"../Library/master.h"
+#include "../Library/master.h"
+
+// sta³e do obs³ugi menu podrêcznego
+
+enum
+{
+	FULL_WINDOW = GL_ALWAYS + 100, // aspekt obrazu - ca³e okno
+	ASPECT_1_1, // aspekt obrazu 1:1
+	EXIT // wyjœcie
+};
+
+// aspekt obrazu
+
+int aspect = FULL_WINDOW;
 
 // rozmiary bry³y obcinania
 
@@ -16,31 +30,10 @@ const GLdouble top = 2.0;
 const GLdouble near_ = 3.0;
 const GLdouble far_ = 7.0;
 
-// sta³e do obs³ugi menu podrêcznego
-
-enum
-{
-	CUTTING_PLANE, // p³aszczyzna przekroju
-	POLYGON_OFFSET, // przesuniêcie wartoœci g³êbi
-	EXIT // wyjœcie
-};
-
-// k¹t obrotu kuli
-
-GLfloat angle = 0.0;
-
-// k¹ty obrotu szeœcianu
+// k¹ty obrotu
 
 GLfloat rotatex = 0.0;
 GLfloat rotatey = 0.0;
-
-// wskaŸnik rysowania p³aszczyzna przekroju
-
-bool cutting_plane = true;
-
-// wskaŸnik przesuniêcia wartoœci g³êbi
-
-bool polygon_offset = true;
 
 // wskaŸnik naciœniêcia lewego przycisku myszki
 
@@ -50,7 +43,64 @@ int button_state = GLUT_UP;
 
 int button_x, button_y;
 
+// rodzaj testu bufora g³êbokoœci
+
+GLenum depth_test = GL_LESS;
+
 // funkcja generuj¹ca scenê 3D
+
+GLfloat vertex[11 * 3] =
+{
+
+	0.0f, 1.0f, 0.0f , // Szczyt góry
+	0.50f, -1.0f, -0.87f , //1
+	-0.50f, -1.0f, -0.87f , // 2
+	-1.0f, -1.0f, 0.0f , // 3
+	-0.50f, -1.0f, 0.87f ,//4
+	0.50f, -1.0f, 0.87f ,//5
+	1.0f, -1.0f, 10.0f ,//6
+	0.50f, -1.0f, -0.87f 
+
+};
+
+int triangles[18 * 3] =
+{
+
+	1, 2, 0,
+	2, 3, 0,
+	3, 4, 0,
+	4, 5, 0,
+	5, 6, 0,
+	6, 7, 0,
+	7, 8, 0,
+	1, 8, 10,
+};
+
+void Normal(GLfloat *n, int i)
+{
+	GLfloat v1[3], v2[3];
+	// obliczenie wektorów na podstawie wspó³rzêdnych wierzcho³ków trójk¹tów
+	v1[0] = vertex[3 * triangles[3 * i + 1] + 0] - vertex[3 * triangles[3 * i + 0] + 0];
+	v1[1] = vertex[3 * triangles[3 * i + 1] + 1] - vertex[3 * triangles[3 * i + 0] + 1];
+	v1[2] = vertex[3 * triangles[3 * i + 1] + 2] - vertex[3 * triangles[3 * i + 0] + 2];
+	v2[0] = vertex[3 * triangles[3 * i + 2] + 0] - vertex[3 * triangles[3 * i + 1] + 0];
+	v2[1] = vertex[3 * triangles[3 * i + 2] + 1] - vertex[3 * triangles[3 * i + 1] + 1];
+	v2[2] = vertex[3 * triangles[3 * i + 2] + 2] - vertex[3 * triangles[3 * i + 1] + 2];
+	// obliczenie waktora normalnego przy pomocy iloczynu wektorowego
+	n[0] = v1[1] * v2[2] - v1[2] * v2[1];
+	n[1] = v1[2] * v2[0] - v1[0] * v2[2];
+	n[2] = v1[0] * v2[1] - v1[1] * v2[0];
+}
+
+// wskaŸnik przesuniêcia wartoœci g³êbi
+bool polygon_offset = true;
+
+// k¹t obrotu kuli
+GLfloat angle = 0.0;
+
+// wskaŸnik rysowania p³aszczyzna przekroju
+bool cutting_plane = true;
+
 
 void Display()
 {
@@ -74,97 +124,74 @@ void Display()
 	glRotatef(rotatey, 0, 1.0, 0);
 
 	// niewielkie powiêkszenie szeœcianu
-	glScalef(1.15, 1.15, 1.15);
+	glScalef(2, 2, 2);
 
 	// w³¹czenie testu bufora g³êbokoœci
 	glEnable(GL_DEPTH_TEST);
+
+	// wybór funkcji do testu bufora g³êbokoœci
+	glDepthFunc(depth_test);
 
 	// najpierw rysujemy kulê obracaj¹c¹ siê wewn¹trz szeœcianu;
 	// z uwagi na celowy brak efektów oœwietlenia, obrót kuli
 	// podkreœla druga kula w wersji "szkieletowej"
 	glPushMatrix();
-	angle += 0.2;
-	glRotatef(angle, 1.0, 1.0, 0.0);
-	glColor3fv(Yellow);
+	angle += 0.1;
+	glRotatef(angle, 0.5, 0.5, 0.0);
+	glColor3fv(Red);
 	if (polygon_offset)
 		glEnable(GL_POLYGON_OFFSET_FILL);
 
-	glPolygonOffset(1.0, 1.0);
-	glutSolidSphere(0.5, 10, 10);
+	glPolygonOffset(0.5, 0.5);
+	glutSolidSphere(0.3, 10, 10);
 	glColor3fv(Black);
-	glutWireSphere(0.5, 10, 10);
+	glutWireSphere(0.3, 10, 10);
 	if (polygon_offset)
 		glDisable(GL_POLYGON_OFFSET_FILL);
 
 	glPopMatrix();
 
-	// w drugiej kolejnoœci rysujemy wnêtrze szeœcianu;
-	// rysowane s¹ tylko przednie strony wewnêtrznych œcian
-	// no i nie jest rysowana œciana przednia, w której bêdzie otwór
-	glEnable(GL_CULL_FACE);
-	glBegin(GL_QUADS);
-	glColor3fv(Blue);
-	glVertex3f(-1.0, -1.0, -1.0);
-	glVertex3f(1.0, -1.0, -1.0);
-	glVertex3f(1.0, 1.0, -1.0);
-	glVertex3f(-1.0, 1.0, -1.0);
-
-	glColor3fv(Lime);
-	glVertex3f(-1.0, 1.0, 1.0);
-	glVertex3f(-1.0, -1.0, 1.0);
-	glVertex3f(-1.0, -1.0, -1.0);
-	glVertex3f(-1.0, 1.0, -1.0);
-
-	glColor3fv(Cyan);
-	glVertex3f(1.0, 1.0, 1.0);
-	glVertex3f(-1.0, 1.0, 1.0);
-	glVertex3f(-1.0, 1.0, -1.0);
-	glVertex3f(1.0, 1.0, -1.0);
-
-	glColor3fv(Green);
-	glVertex3f(1.0, -1.0, 1.0);
-	glVertex3f(1.0, 1.0, 1.0);
-	glVertex3f(1.0, 1.0, -1.0);
-	glVertex3f(1.0, -1.0, -1.0);
-
-	glColor3fv(Cyan);
-	glVertex3f(-1.0, -1.0, 1.0);
-	glVertex3f(1.0, -1.0, 1.0);
-	glVertex3f(1.0, -1.0, -1.0);
-	glVertex3f(-1.0, -1.0, -1.0);
-	glEnd();
-	glDisable(GL_CULL_FACE);
-
 	// rysowanie p³aszczyzny otworu w szeœcianie
+
 	if (cutting_plane)
+
 	{
 		// wy³¹czenie rysowania w buforze kolorów
 		glDrawBuffer(GL_NONE);
-
 		// rysowanie kwadratu czêœciowo ods³aniaj¹cego wnêtrze szeœcianu
 		// (kwadrat jest po³o¿ony o 0,001 jednostki nad bokiem szeœcianu)
 		glBegin(GL_QUADS);
-		glVertex3f(-0.6, -0.6, 1.001);
-		glVertex3f(0.6, -0.6, 1.001);
-		glVertex3f(0.6, 0.6, 1.001);
-		glVertex3f(-0.6, 0.6, 1.001);
+		glVertex3f(-0.5, -1.001f, -0.5);
+		glVertex3f(0.5, -1.001f, -0.5);
+		glVertex3f(0.5, -1.001f, 0.5);
+		glVertex3f(-0.5, -1.001f, 0.5);
 		glEnd();
 
+
 		// w³¹czenie rysowania w buforze kolorów
+
 		glDrawBuffer(GL_BACK);
+
 	}
 
-	// w³aœciwy szeœcian z obramowaniem, którego rysowanie wymusza brak oœwietlenia
-	glColor3fv(Red);
-	if (polygon_offset)
-		glEnable(GL_POLYGON_OFFSET_FILL);
 
-	glPolygonOffset(1.0, 1.0);
-	glutSolidCube(2.0);
-	glColor3fv(Black);
-	glutWireCube(2.0);
-	if (polygon_offset)
-		glDisable(GL_POLYGON_OFFSET_FILL);
+	glBegin(GL_TRIANGLES);
+	for (int i = 0; i < 18; i++)
+	{
+		GLfloat n[3];
+		Normal(n, i);
+		// uœredniony wektor normalny jest normalizowany tylko, gdy biblioteka
+		// obs³uguje automatyczne skalowania jednostkowych wektorów normalnych
+		glNormal3fv(n);
+		glColor3fv(Lime);
+		glVertex3fv(&vertex[3 * triangles[3 * i + 0]]);
+		glColor3fv(BlueViolet);
+		glVertex3fv(&vertex[3 * triangles[3 * i + 1]]);
+		glColor3fv(Yellow);
+		glVertex3fv(&vertex[3 * triangles[3 * i + 2]]);
+	}
+	// koniec definicji szeœcianu RGB
+	glEnd();
 
 	// skierowanie poleceñ do wykonania
 	glFlush();
@@ -187,7 +214,20 @@ void Reshape(int width, int height)
 	glLoadIdentity();
 
 	// parametry bry³y obcinania
-	glFrustum(left, right, bottom, top, near_, far_);
+	if (aspect == ASPECT_1_1)
+	{
+		// wysokoœæ okna wiêksza od wysokoœci okna
+		if (width < height && width > 0)
+			glFrustum(left, right, bottom * height / width, top * height / width, near_, far_);
+		else
+
+			// szerokoœæ okna wiêksza lub równa wysokoœci okna
+			if (width >= height && height > 0)
+				glFrustum(left * width / height, right * width / height, bottom, top, near_, far_);
+
+	}
+	else
+		glFrustum(left, right, bottom, top, near_, far_);
 
 	// generowanie sceny 3D
 	Display();
@@ -262,21 +302,70 @@ void Menu(int value)
 {
 	switch (value)
 	{
-		// p³aszczyzna przekroju
-	case CUTTING_PLANE:
-		cutting_plane = !cutting_plane;
+		// obszar renderingu - ca³e okno
+	case FULL_WINDOW:
+		aspect = FULL_WINDOW;
+		Reshape(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
+		break;
+
+		// obszar renderingu - aspekt 1:1
+	case ASPECT_1_1:
+		aspect = ASPECT_1_1;
+		Reshape(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
+		break;
+
+		// test bufora g³êbokoœci: GL_NEVER
+	case GL_NEVER:
+		depth_test = GL_NEVER;
 		Display();
 		break;
 
-		// przesuniêcie wartoœci g³êbi
-	case POLYGON_OFFSET:
-		polygon_offset = !polygon_offset;
+		// test bufora g³êbokoœci: GL_LESS
+	case GL_LESS:
+		depth_test = GL_LESS;
+		Display();
+		break;
+
+		// test bufora g³êbokoœci: GL_EQUAL
+	case GL_EQUAL:
+		depth_test = GL_EQUAL;
+		Display();
+		break;
+
+		// test bufora g³êbokoœci: GL_LEQUAL
+	case GL_LEQUAL:
+		depth_test = GL_LEQUAL;
+		Display();
+		break;
+
+		// test bufora g³êbokoœci: GL_GREATER
+	case GL_GREATER:
+		depth_test = GL_GREATER;
+		Display();
+		break;
+
+		// test bufora g³êbokoœci: GL_NOTEQUAL
+	case GL_NOTEQUAL:
+		depth_test = GL_NOTEQUAL;
+		Display();
+		break;
+
+		// test bufora g³êbokoœci: GL_GEQUAL
+	case GL_GEQUAL:
+		depth_test = GL_GEQUAL;
+		Display();
+		break;
+
+		// test bufora g³êbokoœci: GL_ALWAYS
+	case GL_ALWAYS:
+		depth_test = GL_ALWAYS;
 		Display();
 		break;
 
 		// wyjœcie
 	case EXIT:
 		exit(0);
+	default:;
 	}
 }
 
@@ -292,7 +381,13 @@ int main(int argc, char * argv[])
 	glutInitWindowSize(500, 500);
 
 	// utworzenie g³ównego okna programu
-	glutCreateWindow("Z-bufor");
+#ifdef WIN32
+
+	glutCreateWindow("Szeœcian 5");
+#else
+
+	glutCreateWindow("Szescian 5");
+#endif
 
 	// do³¹czenie funkcji generuj¹cej scenê 3D
 	glutDisplayFunc(Display);
@@ -312,27 +407,46 @@ int main(int argc, char * argv[])
 	// utworzenie menu podrêcznego
 	glutCreateMenu(Menu);
 
-	// menu g³ówne
-	glutCreateMenu(Menu);
+	// utworzenie podmenu - aspekt obrazu
+	int MenuAspect = glutCreateMenu(Menu);
 #ifdef WIN32
 
-	glutAddMenuEntry("P³aszczyzna przekroju: rysowana/nierysowana", CUTTING_PLANE);
-	glutAddMenuEntry("Przesuniêcie wartoœci g³êbi: w³¹cz/wy³¹cz", POLYGON_OFFSET);
+	glutAddMenuEntry("Aspekt obrazu - ca³e okno", FULL_WINDOW);
+#else
+
+	glutAddMenuEntry("Aspekt obrazu - cale okno", FULL_WINDOW);
+#endif
+
+	glutAddMenuEntry("Aspekt obrazu 1:1", ASPECT_1_1);
+
+	// utworzenie podmenu - Test z-bufora
+	int MenuZbuffer = glutCreateMenu(Menu);
+	glutAddMenuEntry("GL_NEVER", GL_NEVER);
+	glutAddMenuEntry("GL_LESS", GL_LESS);
+	glutAddMenuEntry("GL_EQUAL", GL_EQUAL);
+	glutAddMenuEntry("GL_LEQUAL", GL_LEQUAL);
+	glutAddMenuEntry("GL_GREATER", GL_GREATER);
+	glutAddMenuEntry("GL_NOTEQUAL", GL_NOTEQUAL);
+	glutAddMenuEntry("GL_GEQUAL", GL_GEQUAL);
+	glutAddMenuEntry("GL_ALWAYS", GL_ALWAYS);
+
+	// menu g³ówne
+	glutCreateMenu(Menu);
+	glutAddSubMenu("Aspekt obrazu", MenuAspect);
+	glutAddSubMenu("Test z-bufora", MenuZbuffer);
+#ifdef WIN32
+
 	glutAddMenuEntry("Wyjœcie", EXIT);
 #else
 
-	glutAddMenuEntry("Plaszczyzna przekroju: rysowana/nierysowana", CUTTING_PLANE);
-	glutAddMenuEntry("Przesuniecie wartosci glebi: wlacz/wylacz", POLYGON_OFFSET);
 	glutAddMenuEntry("Wyjscie", EXIT);
 #endif
 
 	// okreœlenie przycisku myszki obs³uguj¹cej menu podrêczne
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
-	// do³¹czenie funkcji wywo³ywanej podczas "bezczynnoœci" systemu
-	glutIdleFunc(Display);
-
 	// wprowadzenie programu do obs³ugi pêtli komunikatów
 	glutMainLoop();
 	return 0;
 }
+
